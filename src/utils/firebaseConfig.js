feb68aa3c6b2ea893efae6c1f182a445e157c089
Brasseur Thibault <t.brasseur@activigie.com>
2021-03-26 15:51:59 +0100

    First commit : Setting up CRUD Start (main.js) => react init App + Stack : firebase ; firebaseui (Authentification)

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCH_uw9SgJbUMiKkKhMYIlND8rkCS1fqUM",
+    authDomain: "react-auth-firebase-eeffc.firebaseapp.com",
+    // projectId: "react-auth-firebase-eeffc",
+    // storageBucket: "react-auth-firebase-eeffc.appspot.com",
+    // messagingSenderId: "921017042201",
+    // appId: "1:921017042201:web:b851f3ff85fc75c6d37188"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+export default firebase;
